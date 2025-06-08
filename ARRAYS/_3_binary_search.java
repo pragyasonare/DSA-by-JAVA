7b88a4b85df180c8fe307ad79dff869c26f9f456
Pragya sonare <pragyasonare86@gmail.com>
2025-06-08 22:18:09 +0530

    new code

--- ARRAYS/_3_binary_search.java
+++ ARRAYS/_3_binary_search.java
@@ -1,5 +1,28 @@
 package ARRAYS;
 
 public class _3_binary_search {
+    public static int binary_search(int numbers[], int key ){
+        int start =0 , end = numbers.length -1 ;
+        while (start<=end ){
+            int mid = (start + end )/2 ;
+//comparison
+            if (numbers[mid] == key ){
+                return mid ;
+            }
+            if (numbers[mid] < key ){
+                start = mid + 1 ;
+            }
+            if (numbers[mid] >key ){
+                end = mid - 1 ;
+            }
+        }
+        return -1 ;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10,12,14};
+        int key = 6;
+        System.out.println(binary_search(numbers,key));
+    }
+
     
 }
--- STRINGS/_3_largest_string.java
+++ STRINGS/_3_largest_string.java
@@ -1,6 +1,20 @@
 package STRINGS;
 
 public class _3_largest_string {
+     public static void main(String[] args) {
+        String fruits[] = {"apple " , "banana" , "mango"};
+        String largest = fruits[0];
+        for (int i =0 ; i<fruits.length ; i++){
+            if ( largest .compareTo(fruits[i]) < 0){
+                //here compareTo. in function which compare string
+                //if string equal then = 0
+                //if str1 <str2 = <0
+                //if str1 >str2 = >0
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest );
+    }
     
     
 }
