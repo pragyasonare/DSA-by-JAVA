dacf76829e401d4a656b6c7d81ec7a2655d8d8ef
Pragya sonare <pragyasonare86@gmail.com>
2025-06-08 22:05:52 +0530

    added linear search

---
+++ ARRAYS/_2_linear_search.java
@@ -0,0 +1,24 @@
+package ARRAYS;
+
+public class _2_linear_search {    public static int linearsearch (int numbers[], int key )
+    {
+        for (int i=0 ; i<numbers.length ; i++ ){
+            if (numbers[i] ==key){
+                return  i ;
+            }
+        }
+        return -1 ;
+    }
+    public static void main(String[] args) {
+        int number[] = {55,88,44,1,99,9,6,5,4,7};
+        int key = 99;
+       int index = linearsearch(number,key);
+       if (index == -1 ){
+           System.out.println("not found key");
+       } else {
+        System.out.println( "the key was found at index "+ index);
+       }
+    }
+
+    
+}
