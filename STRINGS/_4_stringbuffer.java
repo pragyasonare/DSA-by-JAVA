64fbd6f1621d33fefd921a11ff9b31ac732db3e3
Pragya sonare <pragyasonare86@gmail.com>
2025-06-10 16:59:39 +0530

    new

---
+++ STRINGS/_4_stringbuffer.java
@@ -0,0 +1,18 @@
+package STRINGS;
+
+public class _4_stringbuffer {
+        public static void main(String[] args) {
+        StringBuilder ab = new StringBuilder("");
+        for ( char ch = 'a'; ch< 'z' ; ch++){
+            ab.append(ch);
+        }
+        System.out.println(ab);
+
+//        String abc ="";
+//        for (char i = 'a' ; i<'z'; i++){
+//            System.out.println(abc.charAt(i));
+//        }
+//        System.out.println(abc);
+    }
+    
+}
