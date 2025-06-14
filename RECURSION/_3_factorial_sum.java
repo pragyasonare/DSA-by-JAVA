964559424f3bac3b4285a94337e691f226392c43
Pragya sonare <pragyasonare86@gmail.com>
2025-06-14 23:48:30 +0530

    ll

---
+++ RECURSION/_3_factorial_sum.java
@@ -0,0 +1,28 @@
+package RECURSION;
+
+public class _3_factorial_sum {
+       public static int factorial(int n){
+        //print factorial
+        if (n==0){
+            return 1;
+        }
+        int fnm1 = factorial(n-1);
+        int fn = n * fnm1;
+        return fn;
+
+ // sum of n natural number ----
+
+//  if (n==1 ){
+//      return 1;
+//  }
+//  else
+//      return  n+ factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+     int n =5 ;
+        System.out.println(factorial(n));
+    }
+    
+    
+}
