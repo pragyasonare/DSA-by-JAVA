b2a7647bfa67920990a786b414854430a714bbe4
Pragya sonare <pragyasonare86@gmail.com>
2025-06-16 17:08:50 +0530

    reverse number

---
+++ MATHS/_3_reverse_num.java
@@ -0,0 +1,24 @@
+package MATHS;
+
+import java.util.Scanner;
+
+public class _3_reverse_num {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number ");
+        int n = sc.nextInt();
+
+        int rev = 0 ;
+        
+        while(n > 0){
+            rev = (rev * 10) + n % 10 ;
+            n = n / 10 ;
+
+        }
+
+        System.out.println(rev);
+
+    }
+
+}
