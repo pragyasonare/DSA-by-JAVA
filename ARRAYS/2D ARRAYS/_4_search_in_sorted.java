10437854efdebf61c6d03c352d966c6985c69ed1
Pragya sonare <pragyasonare86@gmail.com>
2025-06-21 18:00:08 +0530

    diagonalsum

---
+++ ARRAYS/2D ARRAYS/_4_search_in_sorted.java
@@ -0,0 +1,34 @@
+
+public class _4_search_in_sorted {
+     public static boolean staircase(int matrix[][] ,int key){
+        int row = 0 ;
+        int col = matrix[0].length - 1 ;
+
+        while (row <matrix.length && col >= 0 ){
+            if (matrix[row][col] == key ){
+                System.out.println("key found at (" + row +","+ col + ")" );
+                return true ;
+            }
+            else if (key < matrix[row][col] ) {
+                col--;
+            }
+            else if  (key > matrix[row][col]){
+                row++;
+            }
+        }
+        System.out.println("key not found");
+        return false ;
+    }
+    public static void main(String[] args)
+    {
+        int matrix[][]= {{10,20,30,40},
+                        { 15,25,34,45},
+                         {27,29,37,48},
+                         {32,33,39,50}};
+        int key = 33;
+        staircase(matrix,key);
+
+
+    }
+    
+}
