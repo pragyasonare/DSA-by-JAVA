bc56e472b2fdf2e5379c904cb769b3b0e3acdb0f
Pragya sonare <pragyasonare86@gmail.com>
2025-06-24 23:42:37 +0530

    arraylist

---
+++ ARRAYLIST/_4_pairsum_iteration.java
@@ -0,0 +1,27 @@
+public class _4_pairsum_iteration {
+     public static boolean pairsum1( ArrayList<Integer>list, int target){
+        for (int i =0 ; i<list.size(); i++){
+            for (int j =0 ; j<list.size();j++){
+                if (list.get(i)+list.get(j)== target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        int target =5;
+        System.out.println(pairsum1(list,target));
+
+    }
+    
+}
