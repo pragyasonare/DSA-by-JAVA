62da8886b58a3ecf08ffa6b4e3dd8bdadbfc9513
Pragya sonare <pragyasonare86@gmail.com>
2025-06-26 20:32:59 +0530

    Backstracking Array

---
+++ BACKTRACKING/backstracking_array.java
@@ -0,0 +1,32 @@
+package BACKTRACKING;
+
+public class backstracking_array {
+
+      public static void changeArr(int arr[] , int i , int value){
+
+        //basecase
+        if (i== arr.length){
+            printarr(arr);
+            return;
+        }
+
+        //recursion or kaam
+        arr[i] = value;
+        changeArr(arr,i+1,value+1);
+        arr[i]= arr[i]-2; //backtracking step
+    }
+    public static void printarr(int arr[]){
+        for (int i =0 ; i < arr.length ; i++){
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr [] = new int[5];
+        changeArr(arr, 0 , 1);
+        printarr(arr);
+
+    }
+    
+}
