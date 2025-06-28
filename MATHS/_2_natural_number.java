bbe892f5aa988a8302137b37478f835d479a23eb
Pragya sonare <pragyasonare86@gmail.com>
2025-06-28 22:38:20 +0530

    natural number

---
+++ MATHS/_2_natural_number.java
@@ -0,0 +1,28 @@
+package MATHS;
+
+import java.util.Scanner;
+
+public class _2_natural_number {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i,n;
+        System.out.println("enter upto which u want to print beta");
+        n= sc.nextInt();
+         for (i =1 ; i<=n; i++ )
+        {
+            System.out.println(i);
+        }
+         
+
+//        for (int i=1; i<=20 ; i++ )
+//        {
+//            System.out.println(i);
+//        }
+        
+
+    }
+
+
+    
+}
