9faf3a60b1338d03c1a5a99b82dbbac2f180102e
Pragya sonare <pragyasonare86@gmail.com>
2025-07-04 20:41:58 +0530

    optimised power

---
+++ RECURSION/_9_optimised_power.java
@@ -0,0 +1,25 @@
+public class _9_optimised_power {
+
+     public static int  optimised(int x,int n){
+        if (n == 0) {
+            return 1;
+        }
+        int halfpowersq = optimised(x,n/2);
+
+        int ans = halfpowersq * halfpowersq;
+
+        //if n is odd
+        if (n%2 != 0){
+            ans =  x * ans;
+        }
+        return ans ;
+
+    }
+
+    public static void main(String[] args) {
+        int x=2;
+        int n=10;
+        System.out.println(optimised(x,n));
+    }
+    
+}
