8109ebe4f7dfcecded93ccbb06195b6fb4dea462
Pragya sonare <pragyasonare86@gmail.com>
2025-07-07 19:44:15 +0530

    variables

---
+++ JAVA/_1_variables.java
@@ -0,0 +1,14 @@
+package JAVA;
+
+public class _1_variables {
+        public static void main(String[] args) {
+        System.out.println("the sum of these number is :");
+        int num1=9;
+        int num2=88;
+        int num3=77;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
+    }
+    
+}
