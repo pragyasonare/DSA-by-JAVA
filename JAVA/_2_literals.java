cd180f21121777a0f167606025fceeca2006b9a2
Pragya sonare <pragyasonare86@gmail.com>
2025-07-07 19:45:13 +0530

    literals

---
+++ JAVA/_2_literals.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class _2_literals {
+
+        public static void main(String[] args) {
+        byte age =55;
+        int age2 = 99;
+        short age3 = 87;
+        long ageDino = 566666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 =  4.66D;
+        boolean a = true;
+        System.out.println(age);
+        String  str = "pragya";
+        System.out.println(str);
+    }
+    
+}
