240a47de6778f1c801f149eaa6ba127d0d2772a0
Pragya sonare <pragyasonare86@gmail.com>
2025-07-24 23:18:37 +0530

    kth largest is array

---
+++ ARRAYS/_12_kth_largest_elemet_in_array.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+public class _12_kth_largest_elemet_in_array {
+
+        public int findKthLargest(int[] nums, int k) {
+         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+         for(int num : nums){
+            minHeap.add(num);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+         }
+         return minHeap.peek();
+        
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {3,2,1,5,6,4} ;
+       int k = 2 ;
+       
+    }
+    
+}
