7e3d34d72aeb4be5593185b313cf005951e05b60
Pragya sonare <pragyasonare86@gmail.com>
2025-08-10 20:43:17 +0530

    uncommon string

---
+++ STRINGS/_12_uncommon_string.java
@@ -0,0 +1,25 @@
+public class _12_uncommon_string {
+
+    class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) { 
+        String[] words = (s1 + " " + s2).split(" ");
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String word : words){
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+         List<String> result = new ArrayList<>();
+         for(String word : map.KeySet())  {
+            if(map.get(word) == 1){
+                result.add(word);
+            }
+         }
+
+         return result.toArray(new String[0]);
+
+
+    }
+}
+    
+}
