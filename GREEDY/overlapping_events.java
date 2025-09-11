814067c7e6e9de4e2ed3f59136dff2df639cb535
Pragya sonare <pragyasonare86@gmail.com>
2025-09-11 23:55:32 +0530

    overlapping events

---
+++ GREEDY/overlapping_events.java
@@ -0,0 +1,26 @@
+package GREEDY;
+
+import java.util.Arrays;
+
+public class overlapping_events {
+     public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals.length == 0){
+            return 0;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int count = 0 ;
+        int prevend = intervals[0][1];
+
+        for(int i= 1; i<intervals.length ; i++){
+            if(intervals[i][0] < prevend){
+                count++ ;
+            }
+            else{
+                prevend = intervals[i][1];
+            }
+        }
+         return count ++;
+    }
+    
+}
