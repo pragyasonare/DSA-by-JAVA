ab9e73c3ec98e669b9358b8563a9bc4aeac41fbc
Pragya sonare <pragyasonare86@gmail.com>
2025-09-25 18:04:35 +0530

    guess number

---
+++ leetcode/_374_guess_num.java
@@ -0,0 +1,22 @@
+public class _374_guess_num {
+     public int guessNumber(int n) {
+        int left = 1 , right = n ;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2 ;
+            int res = guess(mid);
+
+            if(res == 0){
+                return mid ;
+
+            }else if (res < 0){
+            right = mid - 1 ;
+            }
+            else {
+                left = mid + 1 ;
+            }
+        }
+        return -1 ;
+    }
+    
+}
