98aeb6563e5686fbf7797462d4e8c0c4c5bd62c6
Pragya sonare <pragyasonare86@gmail.com>
2025-10-06 20:22:39 +0530

    set mismatch

---
+++ leetcode/_645_set_mismatch.java
@@ -0,0 +1,20 @@
+public class _645_set_mismatch {
+
+     public int[] findErrorNums(int[] nums) {
+        int n = nums.length ;
+        int [] count = new int[n + 1] ;
+        int dup = -1  , mis = -1 ;
+
+        for(int num : nums){
+            count[num] ++ ;
+        }
+        
+        for(int i = 1 ; i <= n ; i++){
+            if(count[i] == 2)dup = i ;
+            else if(count[i] == 0)mis = i ;
+        }
+
+        return new int[]{dup , mis} ;
+    }
+    
+}
